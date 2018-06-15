38d002644db54f5b73236e6f8760662dfa66efab
Joachim <jschuler@redhat.com>
2018-06-15 17:15:15 -0400

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import logo from "../../static/assets/ms_empire.jpg";
 
 export default () => (
   <div>
-    <div>Hello world!</div>
+    <div>Hello Gatsby!</div>
     <img src={logo} alt="Logo" />
   </div>
 )
