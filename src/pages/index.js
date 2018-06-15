59b05817b9d9a2d751ca7ee23665fd14b3b39f41
Joachim <jschuler@redhat.com>
2018-06-15 17:12:12 -0400

    image example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import logo from "../../static/assets/ms_empire.jpg";
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <div>Hello world!</div>
+    <img src={logo} alt="Logo" />
+  </div>
+)
